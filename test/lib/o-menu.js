98b91eb40d478b5a007e7ade1fb3c288e7c92cd1
Alexander Elias <alex.steven.elias@gmail.com>
2019-05-20 11:56:58 -0700

    bug fixes and code clean up

--- test/lib/o-menu.js
+++ test/lib/o-menu.js
@@ -3,45 +3,12 @@
 // removeTemplate.setAttribute('class', 'o-panel-remove-icon o-panel-icon');
 // removeTemplate.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z"/></svg>';
 
-// var icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="white">
-// 	<rect width="100" height="10"/>
-// 	<rect width="100" height="10"/>
-// </svg>';
-
-// transform="translate(0, 50) rotate(45, 50, 5)"
-
 var Create = function (name, attributes) {
     attributes = attributes || {};
     var ns = 'http://www.w3.org/2000/svg';
     var el = document.createElementNS(ns, name);
-    for (var n in attributes) {
-        var v = attributes[n];
-        el.setAttribute(n, v);
-    }
+    for (var n in attributes) el.setAttribute(n, attributes[n]);
     return el;
-};
-
-var Transform = function (n, e) {
-    var p = new RegExp(n+'(.*)');
-    var a = e.getAttribute('transform') || '';
-    var v = Array.prototype.slice.call(arguments, 2).join(',');
-    e.setAttribute('transform', a.replace(p, n+'('+v+')'));
-};
-
-var Rotate = function (e, d, x, y) {
-    var c = e.getBBox();
-    x = x + (c.width/2);
-    y = y + (c.height/2);
-    Transform('rotate', e, d, x, y);
-};
-
-var Translate = function (e, x, y) {
-    var c = e.getBBox();
-    console.log(c);
-    x = x + (c.width/2);
-    y = y + (c.height/2);
-    console.log(y);
-    Transform('translate', e, x, y);
 };
 
 export default {
@@ -56,122 +23,61 @@ export default {
 	created: function () {
 		var self = this;
 
-        var menuIcon = Create('svg', {
-            width: '48px',
-            height: '48px',
-            viewBox: '0 0 100 100',
-            // xmlns: 'http://www.w3.org/2000/svg'
-        });
-
-        // x: '50', y: '30', transform: 'translate(-50,-5)'
-        // , x: '50', y: '70', transform: 'translate(-50,-5)'
-        var rectOne = Create('rect', { width: '100', height: '10' });
-        var rectTwo = Create('rect', { width: '100', height: '10' });
-
-        Translate(rectOne, 50, 30);
-        Translate(rectTwo, 50, 70);
-
-        menuIcon.appendChild(rectOne);
-        menuIcon.appendChild(rectTwo);
-
-        var arrowLeft = document.createElement('div');
-        var arrowRight = document.createElement('div');
-
-        var closeA = document.createElement('a');
-        var closeLi = document.createElement('li');
-        var closeText = document.createTextNode('Close');
-
-        var menuTool = document.createElement('div');
-        // var menuIcon = document.createElement('div');
-
-        // var body = self.querySelector('[slot="body"]');
-        var menuListAll = self.querySelectorAll('ul');
-        var menuListMain = menuListAll[0];
-        menuListMain.setAttribute('class', 'menu-list-main');
+        var menuLists = self.querySelectorAll('ul');
+        var menuListMain = menuLists[0];
 
         /* arrows */
+        var arrowLeft = document.createElement('div');
+        var arrowRight = document.createElement('div');
         arrowLeft.setAttribute('class', ' menu-arrow-left');
         arrowRight.setAttribute('class', 'menu-arrow-right');
 
         /* close */
+        var closeA = document.createElement('a');
+        var closeLi = document.createElement('li');
+        var closeText = document.createTextNode('Close');
         closeA.appendChild(arrowLeft.cloneNode(true));
         closeA.appendChild(closeText);
         closeLi.appendChild(closeA);
 
         /* icon */
-        // menuIcon.setAttribute('class', 'menu-icon');
-        // menuIcon.appendChild(document.createElement('div'));
-        // menuIcon.appendChild(document.createElement('div'));
-        // menuIcon.appendChild(document.createElement('div'));
+        var menuIcon = Create('svg', { width: '48px', height: '48px', class: 'o-menu-icon', viewBox: '0 0 100 100' });
+        var rectOne = Create('rect', { width: '100', height: '10', transform: 'translate(0,25)' });
+        var rectTwo = Create('rect', { width: '100', height: '10', transform: 'translate(0,65)' });
 
         menuIcon.addEventListener('click', function () {
         	menuListMain.classList.toggle('active');
-        	self.classList.toggle('active');
-
-            // rectOne.y = 50;
-            // rectTwo.y = 50;
-
-            var coordOne = rectOne.getBBox();
-            var coordTwo = rectTwo.getBBox();
-
-            var xcOne = 50 + (coordOne.width/2);
-            var xcTwo = 50 + (coordTwo.width/2);
-            var ycOne = 50 + (coordOne.height/2);
-            var ycTwo = 50 + (coordTwo.height/2);
-
-            rectOne.setAttribute('transform', 'rotate(-45,'+xcOne+','+ycOne+')');
-            rectTwo.setAttribute('transform', 'rotate(45,'+xcTwo+','+ycTwo+')');
+        	var active = self.classList.toggle('active');
+            if (active) {
+                rectOne.setAttribute('transform', 'rotate(45) translate(15,-5)');
+                rectTwo.setAttribute('transform', 'rotate(-45) translate(-50,60)');
+            } else {
+                rectOne.setAttribute('transform', 'translate(0,25)');
+                rectTwo.setAttribute('transform', 'translate(0,65)');
+            }
         });
 
-        menuTool.appendChild(menuIcon);
+        menuIcon.appendChild(rectOne);
+        menuIcon.appendChild(rectTwo);
+        self.insertBefore(menuIcon, self.firstElementChild);
 
-        menuTool.setAttribute('class', 'menu-tool');
-        self.insertBefore(menuTool, self.firstElementChild);
-
-        // for (var i = 1, l = menuListAll.length; i < l; i++) {
-        // 	var closeLiClone = closeLi.cloneNode(true);
-        //
-        // 	menuListAll[i].insertBefore(closeLiClone, menuListAll[i].firstElementChild);
-        // 	menuListAll[i].setAttribute('class', 'menu-list-sub');
-        //
-        // 	/*
-        // 		close
-        // 	*/
-        // 	closeLiClone.addEventListener('click', function() {
-        //
-        // 		this.parentNode.parentNode.classList.toggle('active');
-        //
-        // 		if (this.parentNode.parentNode.parentNode.className === 'menu-list-sub unactive') {
-        // 			this.parentNode.parentNode.parentNode.classList.toggle('unactive');
-        // 		}
-        //
-        // 	});
-        //
-        // 	/*
-        // 		open
-        // 	*/
-        // 	menuListAll[i].previousElementSibling.appendChild(arrowRight.cloneNode(true));
-        // 	menuListAll[i].previousElementSibling.addEventListener('click', function() {
-        //
-        // 		this.parentNode.classList.toggle('active');
-        //
-        // 		if (this.parentNode.parentNode.className === 'menu-list-sub') {
-        // 			this.parentNode.parentNode.classList.toggle('unactive');
-        // 		}
-        //
-        // 	});
-        //
-        // }
-
+        for (var i = 0, l = menuLists.length; i < l; i++) {
+            var menuList = menuLists[i];
+            // add toggle active event
+        }
     },
 	style: /*css*/`
+    .o-menu-icon {
+        fill: black;
+        display: block;
+    	cursor: pointer;
+        margin: 0.1rem;
+    }
 	:host {
     	z-index: 1;
     	width: 100%;
     	display: block;
-    	text-align: left;
     	position: relative;
-
 	}
     :host a {
     	cursor: pointer;
@@ -204,9 +110,6 @@ export default {
 
     /* transform: translate3d(0%, 0%, 0) scale3d(1, 1, 1); */
 
-    .menu-tool {
-    	width: 100%;
-    }
     :host > ul {
     	z-index: 1;
     	display: inline-block;
@@ -253,96 +156,9 @@ export default {
     }
 
     /*
-    	icon
-    */
-    .menu-icon {
-    	width: 3rem;
-    	height: 3rem;
-    	cursor: pointer;
-    	position: relative;
-    }
-    .menu-icon > div {
-    	width: 3rem;
-    	height: 0.18rem;
-    	position: absolute;
-    	transform-origin: 50% 50%;
-    	background-color: currentColor;
-    }
-
-    /*
-    	icon non-active
-    */
-    .menu-icon > div:nth-child(1) {
-    	transform: translateY(0.6rem);
-    }
-    .menu-icon > div:nth-child(2) {
-    	transform: translateY( calc(1.5rem - (0.18rem/2)) );
-    }
-    .menu-icon > div:nth-child(3) {
-    	transform: translateY( calc(3rem - 0.6rem - 0.18rem) );
-    }
-
-    /*
-    	icon active
-    */
-    :host.active .menu-icon > div:nth-child(1) {
-    	transform:
-    		rotate(45deg)
-    		translate(calc(1.5rem/1.5), calc(1.5rem/1.5));
-    }
-    :host.active .menu-icon > div:nth-child(2) {
-    	transform:
-    		rotate(45deg)
-    		translate(calc(1.5rem/1.5), calc(1.5rem/1.5));
-    }
-    :host.active .menu-icon > div:nth-child(3) {
-    	transform:
-    		rotate(-45deg)
-    		translate(calc(-1.5rem/1.5), calc(1.5rem/1.5));
-    }
-
-    /*
     	mobile
     */
-    @media screen and (min-width: 960px) {
-    	.menu-tool {
-    		display: none;
-    	}
-    	:host ul > li > ul {
-    		transform: translate3d(0%, -50%, 0) scale3d(1, 0, 1);
-    	}
-    	:host > ul > li {
-    		position: relative;
-    	}
-    	/* :host > ul > li > .menu-list-sub .menu-list-sub {
-    		top: 0;
-    	} */
-    	:host li > ul > li:nth-child(1) {
-    		display: none;
-    	}
-    	:host li.active > ul {
-    		transform: translate3d(0%, 0%, 0) scale3d(1, 1, 1);
-    	}
-
-    	:host .unactive {
-    		max-height: 0;
-    	}
-    	:host .unactive > li:not(.active) > a {
-    		margin:0;
-    		height: 0;
-    		padding: 0;
-    		touch-action: none;
-        	pointer-events: none;
-    	}
-    }
-
     @media screen and (max-width: 960px) {
-    	.menu-tool {
-    		display: block;
-    	}
-    	.menu-icon {
-    		margin: auto 1rem;
-    	}
     	:host li {
     		display: block;
     	}
