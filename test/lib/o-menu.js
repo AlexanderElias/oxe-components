eb1b855f2285eb52da3563a215f32916be00e1a1
Alexander Elias <alex.steven.elias@gmail.com>
2019-05-21 15:26:18 -0700

    bug fixes code clean up

--- test/lib/o-menu.js
+++ test/lib/o-menu.js
@@ -12,25 +12,29 @@ export default {
 	created: function () {
 		var self = this;
 
+        var iconColor = self.getAttribute('data-icon') || '#000';
+
         /* icon */
-        var menuIcon = Create('svg', { width: '48px', height: '48px', class: 'o-menu-icon', viewBox: '0 0 100 100' });
-        var rectOne = Create('rect', { width: '100', height: '10', style: 'transform: translate(0,25%)' });
-        var rectTwo = Create('rect', { width: '100', height: '10', style: 'transform: translate(0,65%)' });
+        var icon = Create('svg', { width: '48px', height: '48px', viewBox: '0 0 100 100' });
+        var rectOne = Create('rect', { fill: iconColor, width: '100', height: '10', style: 'transform: translate(0,25%)' });
+        var rectTwo = Create('rect', { fill: iconColor, width: '100', height: '10', style: 'transform: translate(0,65%)' });
 
         // var path = Create('path', { d: 'M0 25 L100 25 L100 35 L0 35 M0 65 L0 65 L100 65 L100 75 L0 75 L0 65' });
-        // menuIcon.appendChild(path);
+        // icon.appendChild(path);
         var mainList = self.querySelector('ul');
 
-        menuIcon.addEventListener('click', function () {
+        icon.addEventListener('click', function () {
             var actives = self.querySelectorAll('ul.active');
             if (actives.length > 1) {
+                rectOne.setAttribute('width', '100');
+                rectTwo.setAttribute('width', '100');
                 rectOne.setAttribute('style', 'transform: rotate(45deg) translate(15%,-5%)');
                 rectTwo.setAttribute('style', 'transform: rotate(-45deg) translate(-50%,60%)');
                 // path.setAttribute('d', 'M2 8 L8 2 L98 92 L92 98 M2 92 L92 2 L98 8 L8 98');
                 actives[actives.length-1].classList.toggle('active');
             } else {
-                var active = mainList.classList.toggle('active');
-            	// var active = self.classList.toggle('active');
+                mainList.classList.toggle('active');
+            	var active = self.classList.toggle('active');
                 if (active) {
                     rectOne.setAttribute('style', 'transform: rotate(45deg) translate(15%,-5%)');
                     rectTwo.setAttribute('style', 'transform: rotate(-45deg) translate(-50%,60%)');
@@ -43,33 +47,32 @@ export default {
             }
         });
 
-        menuIcon.appendChild(rectOne);
-        menuIcon.appendChild(rectTwo);
-        self.insertBefore(menuIcon, self.firstElementChild);
+        icon.appendChild(rectOne);
+        icon.appendChild(rectTwo);
+        self.insertBefore(icon, self.firstElementChild);
 
         var lists = self.querySelectorAll('ul > li > a ~ ul');
         for (var i = 0, l = lists.length; i < l; i++) {
-            lists[i].parentElement.addEventListener('click', function (target) {
+            var ul = lists[i];
+            var a = ul.previousElementSibling;
+
+            // var i = Create('svg', { style:'width: 1rem; height: 1rem; position: absolute;', viewBox: '0 0 100 100' });
+            // var p = Create('path', { d: 'M50 50 L90 0 L95 5 L60 50 M50 50 L90 100 L95 95 L60 50' });
+            // i.appendChild(p);
+            // a.parentElement.insertBefore(i, a);
+            
+            a.addEventListener('click', function (target) {
+                rectOne.setAttribute('width', '50');
+                rectTwo.setAttribute('width', '50');
                 rectOne.setAttribute('style', 'transform: rotate(45deg) translate(45%,15%)');
                 rectTwo.setAttribute('style', 'transform: rotate(-45deg) translate(-25%,45%)');
                 // path.setAttribute('d', 'M50 50 L90 0 L95 5 L60 50 M50 50 L90 100 L95 95 L60 50');
                 target.classList.toggle('active');
-            }.bind(null, lists[i]));
+            }.bind(null, ul));
         }
 
     },
 	style: /*css*/`
-
-    :host rect {
-        transition: transform ease-in-out 300ms;
-    }
-    .o-menu-icon {
-        fill: black;
-        margin: 0.1rem;
-        display: block;
-    	cursor: pointer;
-    	box-sizing: border-box;
-    }
 	:host {
     	z-index: 1;
     	width: 100%;
@@ -77,6 +80,15 @@ export default {
     	position: relative;
     	box-sizing: border-box;
 	}
+    :host > svg {
+        margin: 0.1rem;
+        display: block;
+    	cursor: pointer;
+    	box-sizing: border-box;
+    }
+    :host > svg > rect {
+        transition: transform ease-in-out 150ms, width ease-in-out 150ms;
+    }
     :host a {
     	cursor: pointer;
     	min-width: 100%;
@@ -95,11 +107,12 @@ export default {
     }
     :host ul {
 		top: 48px;
-    	z-index: -1;
-    	min-height: 100%;
+        min-height: 0;
+        max-height: 0;
+        overflow: hidden;
     	list-style: none;
 		position: absolute;
-		transform: translate3d(0%, -50%, 0) scale3d(1, 0, 1);
+        transition: max-height ease-in-out 150ms, min-height ease-in-out 150ms;
     }
     :host li:hover, :host li:focus, :host li:active, :host li:visited {
     	background: rgba(0,0,0,0.1);
@@ -111,10 +124,10 @@ export default {
     :host ul > li > ul {
 		top: 0;
     }
-	/* :host.active > ul */
 	:host ul.active {
     	z-index: 1;
-		transform: translate3d(0%, 0%, 0) scale3d(1, 1, 1);
+    	min-height: 100%;
+        max-height: 90vh;
 	}
 
     /* :host li > ul::before {
